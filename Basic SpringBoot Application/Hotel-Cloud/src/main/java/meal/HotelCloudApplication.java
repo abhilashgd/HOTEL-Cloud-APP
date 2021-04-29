a5d15118e056473c9a36c5de3fe1f89082c92cbd
abhilashgd <anrewgd@gmail.com>
2021-04-29 16:57:32 +0530

    Add files via upload

---
+++ Basic SpringBoot Application/Hotel-Cloud/src/main/java/meal/HotelCloudApplication.java
@@ -0,0 +1,13 @@
+package meal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCloudApplication.class, args);
+	}
+
+}
